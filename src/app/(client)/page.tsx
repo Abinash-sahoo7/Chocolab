0aae66431967e4de14ab6c9424cfe408c874cd49
sahooabinash2003@gmail.com <avinashsahoo2021@gmail.com>
2024-10-20 18:08:05 +0530

    Add header component in Home page

---
+++ src/app/(client)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./_components/Header";
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default HomePage
